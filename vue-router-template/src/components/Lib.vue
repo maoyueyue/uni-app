0d6b212e3c15289b9c7f9cc7f30e1cae95942a90
maoyueyue <1660478286@qq.com>
2019-03-27 09:13:30 +0800

    二级路由

---
+++ vue-router-template/src/components/Lib.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="container">
+		<div class="lib-bar">
+			<router-link to="/topic_lib">题库</router-link>
+			<router-link to="/resource_lib">资源库</router-link>
+			<router-link to="/exercise_lib">活动库</router-link>
+		</div>
+		<div class="lib-content"><router-view /></div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Lib',
+	data() {
+		return {
+			
+		};
+	}
+};
+</script>
+
+<style scoped>
+	.container {
+		display: flex;
+		width: 90%;
+		height: 90%;
+		margin: 0 auto;
+		flex-direction: column;
+	}
+	.lib-bar {
+		height: 10%;
+		display: flex;
+		align-items: center;
+		background-color: #fff;
+		margin-top: 20px;
+		padding-left: 30px;
+	}
+	.lib-content{
+		text-align: left;
+		background-color: #fff;
+		margin-top: 30px;
+		height: 80%;
+		padding-left: 30px;
+	}
+</style>
---
+++ vue-router-template/src/components/ResourceLib.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h2>资源库</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ResourceLib',
+	data() {
+		return {
+			
+		};
+	}
+};
+</script>
+
+<style>
+</style>
